7d42f584d823fdc086e19a869dd789e48c33186b
Alexis <alexiscarreras@hotmail.com>
2021-05-28 15:58:52 -0300

    Header terminado (1er commit)

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AlbumIcon from '@material-ui/icons/Album';
+import Typography from '@material-ui/core/Typography';
+
+const useStyle = makeStyles({
+    root: {
+        backgroundColor: 'black',
+        height: '3rem',
+        display: 'flex',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    titulo: {
+        color: 'white',
+        marginTop: '0.4rem',
+        marginRight: '0.5rem'
+    },
+    icon: {
+        marginRight: '2.5rem'
+    }
+
+});
+
+const Header = () => {
+const classes = useStyle();
+
+
+    return(
+        <div className={classes.root}>
+            <Typography className={classes.titulo} variant="h5" gutterBottom>
+                Featurify
+            </Typography>
+            <AlbumIcon className={classes.icon} fontSize="large" color="secondary"/>
+        </div>
+    )
+}
+
+export default Header;
