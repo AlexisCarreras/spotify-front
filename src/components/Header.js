9eb90277a7e0934939b0abfb8fb8cc11632cbb51
Alexis <alexiscarreras@hotmail.com>
2021-06-01 12:12:52 -0300

    Header terminado

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import AlbumIcon from '@material-ui/icons/Album';
 import Typography from '@material-ui/core/Typography';
 
@@ -8,7 +12,23 @@ const useStyle = makeStyles({
         backgroundColor: 'black',
         height: '3rem',
         display: 'flex',
-        justifyContent: 'flex-end',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    botonMenu:{
+        color: 'white',
+        '&:hover': {
+            color: '#F50057',
+        },
+        marginLeft: '1rem',
+    },
+    item: {
+        '&:hover': {
+            color: '#F50057',
+        },
+    },
+    logo: {
+        display: 'flex',
         alignItems: 'center',
     },
     titulo: {
@@ -22,14 +42,39 @@ const useStyle = makeStyles({
 });
 
 const Header = () => {
-const classes = useStyle();
+    const classes = useStyle();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     return(
         <div className={classes.root}>
-            <Typography className={classes.titulo} variant="h5" gutterBottom>
-                Featurify
-            </Typography>
-            <AlbumIcon className={classes.icon} fontSize="large" color="secondary"/>
+            <IconButton onClick={handleClick} className={classes.botonMenu} aria-label="menu">
+                <MenuIcon fontSize="large"/>
+            </IconButton>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={handleClose} className={classes.item}>Search</MenuItem>
+                <MenuItem onClick={handleClose} className={classes.item}>Favorites</MenuItem>
+            </Menu>
+
+            <div className={classes.logo}>
+                <Typography className={classes.titulo} variant="h5" gutterBottom>
+                    Featurify
+                </Typography>
+                <AlbumIcon className={classes.icon} fontSize="large" color="secondary"/>
+            </div>
         </div>
     )
 }
