79475dd975a5030588cccce9cdcbac4ebaf0391c
FerMossier <mossierfernando@gmail.com>
2021-08-18 15:18:43 -0300

    Comienzo de estilos en el perfil de album

--- src/components/Album/Album.js
+++ src/components/Album/Album.js
@@ -1,25 +1,55 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
+    name:{
+        margin: 0,
+        fontSize: '90px'
+    },
+    album:{
+        color: '#fff',
+    },
+    cabecera:{
+        display: 'flex',
+    },
+    albumImg:{
+        width: '250px',
+        height: '250px',
+    },
+    trackList:{
 
+    },
+    contentImg:{
 
-}));
+    },
+    albumArtist:{
 
+    },
+    totalTracks:{
+        
+    }
 
+});
 
 export default function Album({ id, name, image, type, totalTracks, albumArtist, tracks }) {
+    const classes = useStyles();
     return (
-        <div>
-            <h3>{id}</h3>
-            <h3>{name}</h3>
-            <h3>{image}</h3>
-            <h3>{type}</h3>
-            <h3>{totalTracks}</h3>
-            <h3>{albumArtist}</h3>
-
-            {<ul>
-                {tracks.map((a) => <li>{a.name}</li>)}
-            </ul>}
+        <div className = {classes.album}>
+            <section className = {classes.cabecera}>
+                <div className = {classes.contentImg}>
+                    <img className = {classes.albumImg} src={image} alt={name}></img>
+                </div>
+                <h3 className = {classes.name}>{name}</h3>
+                <h3 className = {classes.albumArtist}>{albumArtist}</h3>
+                <h3 className = {classes.totalTracks}>{totalTracks}</h3>
+            </section>
+            
+            <section className = {classes.trackList}>
+                {<ul>
+                    {tracks.map((a) => <li>{a.name}</li>)}
+                </ul>}
+            </section>
+            
         </div>
+
     )
 }
--- src/components/Album/AlbumProfile.js
+++ src/components/Album/AlbumProfile.js
@@ -1,8 +1,21 @@
 import Album from "./Album";
 import React, { useEffect, useState } from "react";
 import AlbumService from "../../service/AlbumService";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    fondo: {
+        background: 'linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(51,51,51,1) 0%, rgba(0,0,0,1) 100%)',
+        width: '100%',
+        height: '100vh',
+        padding: 0,
+
+    },
+
+});
 
 export default function ArtistProfile({ params }) {
+    const classes = useStyles();
     const [album, setAlbum] = useState();
     const { id } = params;
 
@@ -12,10 +25,10 @@ export default function ArtistProfile({ params }) {
     }, [id]);
 
     return (
-        <div>
+        <div className = {classes.fondo}>
             {
                 album &&
-                <div>
+                <div className = {classes.fondo}>
                     {console.log(album)}
                     {
                         <Album
