9a8e7ed347f005f0dd5e09d814cd99b3af45ba77
AlexisCarreras <alexiscarreras@hotmail.com>
2021-08-19 16:56:22 -0300

    Estilos en el perfil de albums, falta poner los favoritos, y enumerar tracks

--- src/components/Album/Album.js
+++ src/components/Album/Album.js
@@ -16,35 +16,87 @@ const useStyles = makeStyles({
     },
     imgFondo:{
         width: '101.54vh',
-        height: '85vh',
-        filter: 'brightness(10%)',
-        background: 'cover',
+        height: '75vh',
+        filter: 'blur(15px) brightness(40%)',
     },
-    contentImg:{
-
+    contentAlbum:{
+        zIndex: '1',
+        display: 'flex',
+        width: '100%',
+        height: '100%',
+    },
+    contentAlbumImg: {
+        width: '30%',
+        height: '650px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     albumImg:{
-        width: '250px',
-        height: '250px',
+        width: '360px',
+        height: '360px',
+    },
+    contentAlbumTittle:{
+        width: '70%',
+        height: '650px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
     },
     name:{
         margin: 0,
-        fontSize: '90px'
+        paddingBottom: '1rem',
+        fontSize: '60px',
+        color: '#fff',
     },
     albumArtist:{
-        
+        margin: 0,
+        paddingBottom: '0.6rem',
+        fontSize: '30px',
+        color: '#fff',
     },
     totalTracks:{
-        
+        margin: 0,
+        fontSize: '18px',
+        color: '#fff',
     },
-    
-    trackList:{
 
+    trackList:{
+        marginTop: '7rem',
+        display: 'flex',
+        justifyContent: 'center',
+    },
+    listaTitulos: {
+        fontSize: '15px',
+        color: '#888',
+        listStyle: 'none',
+        marginBottom: '2rem',
+    },
+    listaTopTrack: {
+        fontSize: '18px',
+        color: '#FFFF',
+        marginRight: '4rem',
+        listStyle: 'none',
+        marginBottom: '1rem',
+        height: '50px',
+    },
+    preview: {
+        fontSize: '15px',
+        color: '#888',
+        marginLeft: '5rem',
+        listStyle: 'none',
+        marginBottom: '0rem',
+    },
+    audio: {
+        display: 'block',
+    	height: '66px',
+        margin: 0,
     },
 });
 
 export default function Album({ id, name, image, type, totalTracks, albumArtist, tracks }) {
     const classes = useStyles();
+
     return (
         <div className = {classes.album}>
             <section className = {classes.cabecera}>
@@ -52,20 +104,46 @@ export default function Album({ id, name, image, type, totalTracks, albumArtist,
                     <img src={image} className={classes.imgFondo} alt={image}></img>
                     <img src={image} className={classes.imgFondo} alt={image}></img>
                 </div>
-                <div className = {classes.contentImg}>
-                    <img className = {classes.albumImg} src={image} alt={name}></img>
-                </div>
-                <h3 className = {classes.name}>{name}</h3>
-                <h3 className = {classes.albumArtist}>{albumArtist}</h3>
-                <h3 className = {classes.totalTracks}>{totalTracks}</h3>
+                <article className = {classes.contentAlbum}>
+                    <div className = {classes.contentAlbumImg}>
+                        <img className = {classes.albumImg} src={image} alt={name}></img>
+                    </div>
+                    <div className = {classes.contentAlbumTittle}>
+                        <h2 className = {classes.name}>{name}</h2>
+                        <h3 className = {classes.albumArtist}>{albumArtist}</h3>
+                        <h4 className = {classes.totalTracks}>{totalTracks} songs</h4>
+                    </div>
+                </article>
             </section>
             
             <section className = {classes.trackList}>
                 {<ul>
-                    {tracks.map((a) => <li>{a.name}</li>)}
+                    <h6 className={classes.listaTitulos}>TITULO</h6>
+                    {tracks.map((a) => 
+                    <li className={classes.listaTopTrack}  key={a.id}>{a.name}</li>)}
                 </ul>}
+                {<ul>
+                    <h6 className={classes.listaTitulos}>ARTISTA</h6>
+                    {tracks.map((a) => 
+                    <li className={classes.listaTopTrack} key={a.id}>{albumArtist}</li>)}
+                </ul>}
+                {<ul>
+                    <h6 className={classes.listaTitulos}>ALBUM</h6>
+                    {tracks.map((a) => 
+                    <li className={classes.listaTopTrack} key={a.id}>{name}</li>)}
+                </ul>}
+                {<ul>
+                    <h6 className={classes.listaTitulos}>DUR.</h6>
+                    {tracks.map((a) => 
+                    <li className={classes.listaTopTrack} key={a.id}>{a.trackLength}</li>)}
+                </ul>}
+                {<ul>
+                    <h6 className={classes.preview}>PREVIEW.</h6>
+                    {tracks.map((a) => 
+                    <audio className={classes.audio} controls key={a.id} src={a.previewUrl}></audio>)}
+                </ul>}
+                {console.log(tracks)}
             </section>
-            
         </div>
 
     )
